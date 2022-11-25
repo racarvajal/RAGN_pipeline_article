1659b37c9dfc660052e837e70e7e35ab79f2a5ca
racarvajal <rcarvajalp@outlook.com>
2022-11-25 15:46:12 +0000

    reduce width white contours

--- src/scripts/fig_W1_W2_W3_conf_matrix_AGN.py
+++ src/scripts/fig_W1_W2_W3_conf_matrix_AGN.py
@@ -169,7 +169,7 @@ for count, idx_ax in enumerate(np.array([[0, 0], [0, 1], [1, 0], [1, 1]])):
     .add_chain([x_axis_dens_AGN_HETDEX[count], y_axis_dens_AGN_HETDEX[count]], parameters=['r_z', 'W1_W2'], name='HETDEX MQC AGN')\
     .add_chain([x_axis_dens_AGN_S82[count], y_axis_dens_AGN_S82[count]], parameters=['r_z', 'W1_W2'], name='Stripe 82 MQC AGN')\
     .configure(shade=False, colors='#FFFFFF', sigmas=contour_levels,\
-               linewidths=4.0, shade_alpha=0.1, sigma2d=False, linestyles='-')\
+               linewidths=2.75, shade_alpha=0.1, sigma2d=False, linestyles='-')\
     .plotter.plot_contour(ax=axs[count], parameter_x='r_z', parameter_y='W1_W2')  # Green AGN
     
     corner_color = ChainConsumer()\
