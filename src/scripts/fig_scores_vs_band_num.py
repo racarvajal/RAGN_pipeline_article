c00554cf756d33c6e767090f21c25f6af872165c
racarvajal <rcarvajalp@outlook.com>
2022-12-05 12:14:41 +0000

    Three scores at a time

--- src/scripts/fig_scores_vs_band_num.py
+++ src/scripts/fig_scores_vs_band_num.py
@@ -22,26 +22,50 @@ catalog_HETDEX_df = pd.read_hdf(file_name_HETDEX, key='df').loc[:, feats_2_use]
 catalog_HETDEX_df = catalog_HETDEX_df.set_index(keys=['ID'])
 catalog_HETDEX_df = catalog_HETDEX_df.loc[test_idx]
 
-score_to_use      = 'Prob_AGN'  # 'Prob_AGN', 'Prob_radio', 'pred_Z'
+score_to_use_1    = 'Prob_AGN'  # 'Prob_AGN', 'Prob_radio', 'pred_Z'
+score_to_use_2    = 'Prob_radio'  # 'Prob_AGN', 'Prob_radio', 'pred_Z'
+score_to_use_3    = 'pred_Z'  # 'Prob_AGN', 'Prob_radio', 'pred_Z'
 
 # filter_rAGN = np.array(catalog_HETDEX_df.loc[:, 'pred_prob_class'] == 1) &\
 #               np.array(catalog_HETDEX_df.loc[:, 'pred_prob_radio'] == 1)
-filter_rAGN = np.ones_like(catalog_HETDEX_df.loc[:, score_to_use]).astype(bool)
+filter_rAGN = np.ones_like(catalog_HETDEX_df.loc[:, score_to_use_1]).astype(bool)
 
-fig             = plt.figure(figsize=(7.1,5.8))
-ax1             = fig.add_subplot(111, projection='scatter_density', xscale='linear', yscale='linear')
-
-_ = gf.plot_scores_band_num(catalog_HETDEX_df.loc[filter_rAGN, 'band_num'],
-                            catalog_HETDEX_df.loc[filter_rAGN, score_to_use], 
-                            ax_pre=ax1, title=None, dpi=10, show_clb=True, log_stretch=False)
+fig             = plt.figure(figsize=(10, 10))
+grid            = fig.add_gridspec(ncols=1, nrows=3, height_ratios=[1, 1, 1],
+                                   hspace=0.0, wspace=0.0)
+axs             = {}
+axs[0]          = fig.add_subplot(grid[0, 0], projection='scatter_density', xscale='linear', yscale='linear')
+axs[1]          = fig.add_subplot(grid[1, 0], projection='scatter_density', xscale='linear', yscale='linear', sharex=axs[0])
+axs[2]          = fig.add_subplot(grid[2, 0], projection='scatter_density', xscale='linear', yscale='linear', sharex=axs[0])
 
 vals_band_num = np.unique(catalog_HETDEX_df.loc[filter_rAGN, 'band_num'])
-mean_scores   = np.zeros_like(vals_band_num, dtype=float)
-std_scores    = np.zeros_like(vals_band_num, dtype=float)
-for count, num in enumerate(vals_band_num):
-    filter_band_n      = np.array(catalog_HETDEX_df.loc[filter_rAGN, 'band_num'] == num)
-    mean_scores[count] = np.nanmean(catalog_HETDEX_df.loc[filter_rAGN * filter_band_n, score_to_use])
-    std_scores[count]  = np.nanstd(catalog_HETDEX_df.loc[filter_rAGN * filter_band_n, score_to_use])
-ax1.errorbar(vals_band_num, mean_scores, yerr=std_scores, ls='None')
+
+boxprops      = dict(linewidth=2.0)
+whiskerprops  = dict(linewidth=2.0)
+capprops      = dict(linewidth=2.0)
+meanprops     = dict(linewidth=2.0)
+medianprops   = dict(linewidth=2.0)
+
+ylabels       = ['$\mathrm{AGN\:prob.}$', '$\mathrm{Radio\:prob.}$', '$\mathrm{Redshift}$']
+
+for count, score_to_use in enumerate([score_to_use_1, score_to_use_2, score_to_use_3]):
+    _ = gf.plot_scores_band_num(catalog_HETDEX_df.loc[filter_rAGN, 'band_num'],
+                            catalog_HETDEX_df.loc[filter_rAGN, score_to_use], 
+                            ax_pre=axs[count], title=None, dpi=5, show_clb=True, 
+                            log_stretch=False, xlabel='$\mathtt{band\_num}$', 
+                            ylabel=ylabels[count])
+
+    all_scores    = []
+    mean_scores   = np.zeros_like(vals_band_num, dtype=float)
+    std_scores    = np.zeros_like(vals_band_num, dtype=float)
+    for count_b, num in enumerate(vals_band_num):
+        filter_band_n      = np.array(catalog_HETDEX_df.loc[filter_rAGN, 'band_num'] == num)
+        all_scores.append(catalog_HETDEX_df.loc[filter_rAGN * filter_band_n, score_to_use])
+        mean_scores[count_b] = np.nanmean(catalog_HETDEX_df.loc[filter_rAGN * filter_band_n, score_to_use])
+        std_scores[count_b]  = np.nanstd(catalog_HETDEX_df.loc[filter_rAGN * filter_band_n, score_to_use])
+
+    axs[count].boxplot(all_scores, positions=vals_band_num, showfliers=False, showmeans=True, 
+                       meanline=True, widths=0.4, boxprops=boxprops, whiskerprops=whiskerprops, 
+                       capprops=capprops, meanprops=meanprops, medianprops=medianprops)
 
 plt.savefig(paths.figures / 'prob_class_band_num_rAGN_HETDEX_test.pdf', bbox_inches='tight')
--- src/scripts/global_functions.py
+++ src/scripts/global_functions.py
@@ -213,7 +213,8 @@ def plot_redshift_compare(true_z, predicted_z, ax_pre, title=None, dpi=10, cmap=
     return ax_pre
 
 # Plot predicted scores (or Z) vs number of measured bands per source
-def plot_scores_band_num(pred_scores, band_num, ax_pre, title=None, dpi=10, cmap=gv.cmap_z_plots, show_clb=False, log_stretch=False):
+def plot_scores_band_num(pred_scores, band_num, ax_pre, title=None, dpi=10, cmap=gv.cmap_z_plots, 
+                         show_clb=False, log_stretch=False, xlabel=None, ylabel=None):
     if log_stretch:
         norm = ImageNormalize(vmin=0., stretch=LogStretch())
     if not log_stretch:
@@ -233,20 +234,21 @@ def plot_scores_band_num(pred_scores, band_num, ax_pre, title=None, dpi=10, cmap
 
     dens_1 = ax_pre.scatter_density(pred_scores.sample(frac=1, random_state=gv.seed), 
                                     band_num.sample(frac=1, random_state=gv.seed), 
-                                    cmap=cmap_mod, zorder=0, dpi=dpi, norm=norm, alpha=0.96)
+                                    cmap=cmap_mod, zorder=0, dpi=dpi, norm=norm, alpha=1.0)
     
     # ax_pre.axline((2., 2.), (3., 3.), ls='--', marker=None, c='Gray', alpha=0.8, lw=3.0, zorder=20)
     # ax_pre.axline(xy1=(1., 1.15), xy2=(2., 2.3), ls='-.', marker=None, c='slateblue', alpha=0.6, lw=3.0, zorder=20)
     # ax_pre.axline(xy1=(1., 0.85), xy2=(2., 1.7), ls='-.', marker=None, c='slateblue', alpha=0.6, lw=3.0, zorder=20)
 
     if show_clb:
-        clb = plt.colorbar(dens_1, extend='neither', norm=norm, ticks=mtick.MaxNLocator(integer=True))
+        clb = plt.colorbar(dens_1, extend='neither', norm=norm, 
+                           ticks=mtick.MaxNLocator(nbins=5, integer=True, prune='lower'))
         clb.ax.tick_params(labelsize=26)
         clb.outline.set_linewidth(2.5)
-        clb.ax.set_ylabel('Elements per pixel', size=28, path_effects=pe2)
+        clb.ax.set_ylabel('Sources per pixel', size=28, path_effects=pe2)
 
-    ax_pre.set_xlabel('$\mathtt{band\_num}$', fontsize=32)
-    ax_pre.set_ylabel('$\mathrm{Score}$', fontsize=32)
+    ax_pre.set_xlabel(xlabel, fontsize=30)
+    ax_pre.set_ylabel(ylabel, fontsize=30)
     ax_pre.tick_params(which='both', top=True, right=True, direction='in')
     ax_pre.tick_params(axis='both', which='minor', labelsize=24.5)
     ax_pre.tick_params(axis='both', which='major', labelsize=24.5)
@@ -260,9 +262,9 @@ def plot_scores_band_num(pred_scores, band_num, ax_pre, title=None, dpi=10, cmap
     ax_pre.yaxis.set_minor_formatter(mtick.ScalarFormatter(useMathText=False))
     plt.setp(ax_pre.spines.values(), linewidth=2.5)
     plt.setp(ax_pre.spines.values(), linewidth=2.5)
-    ax_pre.set_xlim(left=np.floor(min_x)-1, right=np.ceil(max_x))
-    # ax_pre.set_ylim(bottom=np.floor(min_y), top=np.ceil(max_y))
-    ax_pre.set_ylim(bottom=-0.1, top=1.1)
+    ax_pre.set_xlim(left=np.floor(min_x)-0.5, right=np.ceil(max_x)+0.5)
+    ax_pre.set_ylim(bottom=np.floor(min_y)-0.1, top=np.ceil(max_y)+0.1)
+    #ax_pre.set_ylim(bottom=-0.1, top=1.1)
     ax_pre.set_title(title, fontsize=22)
     plt.tight_layout()
     return ax_pre
