884ad43cd3c7331468076c0eb7c4cbcb7116b3c5
racarvajal <rcarvajalp@outlook.com>
2023-05-25 19:01:31 +0100

    save plot with separate scores

--- src/scripts/fig_scores_vs_band_num_sep_class.py
+++ src/scripts/fig_scores_vs_band_num_sep_class.py
@@ -7,6 +7,7 @@ import cmasher as cmr
 import pandas as pd
 import paths
 import global_functions as gf
+import global_variables as gv
 
 mpl.rcdefaults()
 plt.rcParams['text.usetex'] = True
@@ -56,6 +57,7 @@ bottom_plots  = [False, False, True]
 bins_x        = [1.8, 2.2, 2.8, 3.2, 3.8, 4.2, 4.8, 5.2, 5.8, 
                  6.2, 6.8, 7.2, 7.8, 8.2, 8.8, 9.2, 9.8, 10.2, 
                  10.8, 11.2, 11.8, 12.2]
+cal_thresh    = {0: gv.cal_AGN_thresh, 1: gv.cal_radio_thresh}
 
 for count, score_to_use in enumerate([score_to_use_1, score_to_use_2, score_to_use_3]):
     bins_y        = np.linspace(np.nanmin(catalog_HETDEX_df.loc[filter_rAGN[count], score_to_use]), 
@@ -67,40 +69,65 @@ for count, score_to_use in enumerate([score_to_use_1, score_to_use_2, score_to_u
                             show_clb=True, log_stretch=False, xlabel=xlabels[count], 
                             ylabel=ylabels[count], top_plot=top_plots[count], 
                             bottom_plot=bottom_plots[count])
+    
+    if count in [0, 1]:
+        filt_up_score     = np.array(catalog_HETDEX_df.loc[:, score_to_use] >= cal_thresh[count])
+        filt_low_score    = np.array(catalog_HETDEX_df.loc[:, score_to_use] < cal_thresh[count])
+        vals_band_num_up  = np.unique(catalog_HETDEX_df.loc[filter_rAGN[count] * filt_up_score, 
+                                                            'band_num'])
+        vals_band_num_low = np.unique(catalog_HETDEX_df.loc[filter_rAGN[count] * filt_low_score, 
+                                                            'band_num'])
 
-    vals_band_num = np.unique(catalog_HETDEX_df.loc[filter_rAGN[count], 'band_num'])
+        all_scores_up     = []
+        all_scores_low    = []
+        for count_b, num in enumerate(vals_band_num_up):
+            filter_band_n      = np.array(catalog_HETDEX_df.loc[:, 'band_num'] == num)
+            all_scores_up.append(catalog_HETDEX_df.loc[filt_up_score * filter_band_n, score_to_use])
+        for count_b, num in enumerate(vals_band_num_low):
+            filter_band_n      = np.array(catalog_HETDEX_df.loc[:, 'band_num'] == num)
+            all_scores_low.append(catalog_HETDEX_df.loc[filt_low_score * filter_band_n, score_to_use])
 
-    all_scores    = []
-    for count_b, num in enumerate(vals_band_num):
-        filter_band_n      = np.array(catalog_HETDEX_df.loc[:, 'band_num'] == num)
-        all_scores.append(catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, score_to_use])
-
-    axs[count].boxplot(all_scores, positions=vals_band_num, showfliers=False, showmeans=True, 
+        axs[count].boxplot(all_scores_up, positions=vals_band_num_up, showfliers=False, showmeans=True, 
                        meanline=True, widths=0.4, boxprops=boxprops, whiskerprops=whiskerprops, 
                        capprops=capprops, meanprops=meanprops, medianprops=medianprops)
-    for count_b, num in enumerate(vals_band_num):
-        filter_band_n      = np.array(catalog_HETDEX_df.loc[:, 'band_num'] == num)
-        if count in [0, 1]:
-            tmp_conf_mat = gf.conf_mat_func(catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
-                                                                  true_value[count]], 
-                                            catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
-                                                                  predicted_value[count]])
-            tmp_metric   = gf.Recall_from_CM(tmp_conf_mat)
-        if count == 2:
-            tmp_metric   = gf.sigma_nmad(catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
-                                                               true_value[count]], 
-                                         catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
-                                                               predicted_value[count]])
-        axs[count].annotate(text=f'{tmp_metric:.2f} -'.replace('nan', ' '), 
-                            xy=(num, 0.7 * np.nanmax(np.hstack(all_scores))), 
-                            xycoords='data', fontsize=18, ha='center', va='top',  rotation='vertical', 
-                            path_effects=gf.pe2)
-        axs[count].annotate(text=f'{np.sum(filter_rAGN[count] * filter_band_n):,}'.replace(',', '$\,$'), 
-                            xy=(num, 0.935 * np.nanmax(np.hstack(all_scores))), 
-                            xycoords='data', fontsize=18, ha='center', va='top', 
-                            rotation='vertical', path_effects=gf.pe2)
+        axs[count].boxplot(all_scores_low, positions=vals_band_num_low, showfliers=False, showmeans=True, 
+                       meanline=True, widths=0.4, boxprops=boxprops, whiskerprops=whiskerprops, 
+                       capprops=capprops, meanprops=meanprops, medianprops=medianprops)
+
+    if count in [2]:
+        vals_band_num = np.unique(catalog_HETDEX_df.loc[filter_rAGN[count], 'band_num'])
+
+        all_scores    = []
+        for count_b, num in enumerate(vals_band_num):
+            filter_band_n      = np.array(catalog_HETDEX_df.loc[:, 'band_num'] == num)
+            all_scores.append(catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, score_to_use])
+
+        axs[count].boxplot(all_scores, positions=vals_band_num, showfliers=False, showmeans=True, 
+                            meanline=True, widths=0.4, boxprops=boxprops, whiskerprops=whiskerprops, 
+                            capprops=capprops, meanprops=meanprops, medianprops=medianprops)
+    # for count_b, num in enumerate(vals_band_num):
+    #     filter_band_n      = np.array(catalog_HETDEX_df.loc[:, 'band_num'] == num)
+    #     if count in [0, 1]:
+    #         tmp_conf_mat = gf.conf_mat_func(catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
+    #                                                               true_value[count]], 
+    #                                         catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
+    #                                                               predicted_value[count]])
+    #         tmp_metric   = gf.Recall_from_CM(tmp_conf_mat)
+    #     if count == 2:
+    #         tmp_metric   = gf.sigma_nmad(catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
+    #                                                            true_value[count]], 
+    #                                      catalog_HETDEX_df.loc[filter_rAGN[count] * filter_band_n, 
+    #                                                            predicted_value[count]])
+    #     axs[count].annotate(text=f'{tmp_metric:.2f} -'.replace('nan', ' '), 
+    #                         xy=(num, 0.7 * np.nanmax(np.hstack(all_scores))), 
+    #                         xycoords='data', fontsize=18, ha='center', va='top',  rotation='vertical', 
+    #                         path_effects=gf.pe2)
+    #     axs[count].annotate(text=f'{np.sum(filter_rAGN[count] * filter_band_n):,}'.replace(',', '$\,$'), 
+    #                         xy=(num, 0.935 * np.nanmax(np.hstack(all_scores))), 
+    #                         xycoords='data', fontsize=18, ha='center', va='top', 
+    #                         rotation='vertical', path_effects=gf.pe2)
 
 axs[0].set_xlim(left=1.5, right=12.5)
 axs[2].set_xticks(np.unique(catalog_HETDEX_df.loc[:, 'band_num']))
 axs[2].set_xticklabels(np.unique(catalog_HETDEX_df.loc[:, 'band_num']))
-plt.savefig(paths.figures / 'predicted_probs_band_num_HETDEX_test.pdf', bbox_inches='tight')
+plt.savefig(paths.figures / 'predicted_probs_band_num_HETDEX_test_sep_class.pdf', bbox_inches='tight')
